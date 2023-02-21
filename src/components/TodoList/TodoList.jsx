dca6d5af094bd4c122baa7405845a87cf3720723
HackISTP <hyeminpak@gmail.com>
2023-02-21 18:01:54 +0900

    :sparkles:	ADD todo 추가 기능구현

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: "123", text: "장보기", status: "active" },
     { id: "124", text: "공부하기", status: "active" },
   ]);
+
+  const handleAdd = (todo) => {
+    //새로운 투두받아서
+    setTodos([...todos, todo]);
+  };
+
   return (
     <section>
       <ul>
@@ -13,6 +20,7 @@ export default function TodoList() {
           <li key={item.id}>{item.text}</li>
         ))}
       </ul>
+      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
