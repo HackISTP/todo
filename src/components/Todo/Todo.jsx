34c128cb7f1aa4f1f9f51324d5451e0d0a56b2ab
HackISTP <hyeminpak@gmail.com>
2023-02-22 20:03:32 +0900

    :lipstick:	ADD todo item 스타일 꾸미기

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsTrash } from "react-icons/bs";
+import styles from "./Todo.module.css";
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { text, status } = todo;
   const handleChange = (e) => {
@@ -8,17 +9,22 @@ export default function Todo({ todo, onUpdate, onDelete }) {
   };
   const handleDelete = () => onDelete(todo);
   return (
-    <li>
+    <li className={styles.todo}>
       <input
+        className={styles.checkbox}
         type="checkbox"
         id="checkbox"
         checked={status === "completed"}
         onChange={handleChange}
       />
-      <label htmlFor="checkbox">{text}</label>
-      <button onClick={handleDelete}>
-        <BsTrash />
-      </button>
+      <label htmlFor="checkbox" className={styles.text}>
+        {text}
+      </label>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
+          <BsTrash />
+        </button>
+      </span>
     </li>
   );
 }
